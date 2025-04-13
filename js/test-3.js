a4bbd43d4e79ef9e3940ae81428f2e2ec1aba300
semicvetik <semikirina@ukr.net>
2025-04-13 21:34:17 +0200

    add test

---
+++ js/test-3.js
@@ -0,0 +1,28 @@
+// Створи змінну name і присвой їй твоє ім’я. Виведи привітання: "Привіт, [name]!".
+
+// const name = "Iryna";
+// console.log(`Привіт ${name}`);
+
+// Створи функцію sum(a, b), яка повертає суму двох чисел.
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(3, 2));
+
+// Напиши функцію, яка приймає число і повертає "Парне" або "Непарне".
+
+// function evenNumber(number) {
+// if (number%2===0) {
+//     return "Парне";
+// } else {
+//     return "Не парне";
+// }
+// }
+
+// console.log(evenNumber(12));
+// console.log(evenNumber(35));
+// console.log(evenNumber("Hello"));
+
+
