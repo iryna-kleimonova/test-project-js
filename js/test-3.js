a0d687acefe7e0de4309ad7e2e70f874ed942578
semicvetik <semikirina@ukr.net>
2025-04-20 09:23:38 +0200

    add

--- js/test-3.js
+++ js/test-3.js
@@ -63,3 +63,8 @@
 // console.log(objectNaming({name: "Apple", age: 12,}));
 
 // =========================================
+// console.log(_.sum([4, 2, 8, 6])); // 20
+// console.log(_.sum([5, 10])); // 15
+
+// console.log(_.shuffle([1, 2, 3, 4])); // [4, 1, 3, 2]
+// console.log(_.shuffle([1, 2, 3, 4])); // [3, 2, 1, 4]
